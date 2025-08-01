be3432cb0c2ffc58475f62d0c4cf827bce70e8df
SpoonKa <spoon1998spoon@gmail.com>
2025-08-01 04:35:33 +0200

    Try  + ( No Anim )

---
+++ Esc_Dots_Pr/Assets/Scripts/Animator/AnimatorMonoBeh/BoneAuthoring.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class BoneAuthoring : MonoBehaviour
+{
+    [Tooltip("Оставь пустым, чтобы использовать имя GameObject")]
+    public string OverrideBoneName;
+}
+
+public class BoneAuthoringBaker : Baker<BoneAuthoring>
+{
+    public override void Bake(BoneAuthoring authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+        var boneName = string.IsNullOrEmpty(authoring.OverrideBoneName)
+            ? authoring.gameObject.name
+            : authoring.OverrideBoneName;
+
+        AddComponent(entity, new BoneTag
+        {
+            BoneName = new FixedString64Bytes(boneName)
+        });
+
+        AddComponent(entity, new BoneSyncData
+        {
+            LocalPosition = float3.zero,
+            LocalRotation = quaternion.identity
+        });
+
+        // Добавляем Transform для доступа к MonoBehaviour
+        AddComponentObject(entity, authoring.transform);
+
+        // Не забудь: BoneSyncBehaviour должен быть на этом же объекте
+        var syncBehaviour = authoring.GetComponent<BoneSyncBehaviour>();
+        if (syncBehaviour != null)
+        {
+            AddComponentObject(entity, syncBehaviour);
+        }
+    }
+}
+public struct BoneTag : IComponentData
+{
+    public FixedString64Bytes BoneName;
+}
+
+public struct BoneSyncData : IComponentData
+{
+    public float3 LocalPosition;
+    public quaternion LocalRotation;
+}
--- Esc_Dots_Pr/Assets/Scripts/Animator/ServerBoneControlSystem.cs
+++ Esc_Dots_Pr/Assets/Scripts/Animator/ServerBoneControlSystem.cs
@@ -1,33 +1,19 @@
 ﻿using Unity.Entities;
-using Unity.Transforms;
-using Unity.Mathematics;
-using Unity.NetCode;
 
-[UpdateInGroup(typeof(SimulationSystemGroup))]
-[WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
-public partial struct ServerBoneControlSystem : ISystem
+public partial struct ServerBoneAnimationSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (buffer, entity) in SystemAPI
-              .Query<DynamicBuffer<LinkedEntityGroup>>()
-              .WithAll<PlayerInfo>()
-              .WithEntityAccess())
+        foreach (var (animState, entity) in SystemAPI.Query<RefRO<AnimationStateData>>().WithEntityAccess())
         {
-            foreach (var linked in buffer)
+            // Здесь логика: какая кость, какое положение
+            var boneSync = new BoneSyncData
             {
-                if (!SystemAPI.HasComponent<BoneTag>(linked.Value))
-                    continue;
+              
+            };
 
-                var boneName = SystemAPI.GetComponent<BoneTag>(linked.Value).BoneName;
-
-                if (boneName == "mixamorig:Spine1")
-                {
-                    var transform = SystemAPI.GetComponentRW<LocalTransform>(linked.Value);
-                    transform.ValueRW = transform.ValueRW.RotateY(0.01f);
-                }
-            }
+            state.EntityManager.SetComponentData(entity, boneSync);
         }
-
     }
 }
+
